8410b75e2cdbce36be76785ad7606b3c2edbb2a4
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-07-10 15:43:41 +0100

    Delete yarn lock

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const postRouter = require('./posts/postRouter');
 const userRouter = require('./users/userRouter');
 const server = express();
 
 server.use(express.json());
+server.use(cors())
 server.use(logger);
 server.use('/api/posts', postRouter);
 server.use('/api/users', userRouter);
