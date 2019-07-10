40ca27e7aee5ffef29ed3cc48d1fd42525aa0f32
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-07-10 14:24:33 +0100

    Add update and delete posts functionality

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,51 +1,77 @@
-const express = require('express');
+const express = require("express");
 const Posts = require("./postDb");
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    try {
-        const posts = await Posts.get();
-        res.status(200).json(posts);
-    } catch {
-        res.status(500).json({message: 'The posts could not be retrieved'})
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Posts.get();
+    res.status(200).json(posts);
+  } catch {
+    res.status(500).json({ message: "The posts could not be retrieved" });
+  }
+});
+
+router.get("/:id", validatePostId, async (req, res) => {
+  try {
+    const post = await Posts.getById(req.post.id);
+    res.status(200).json(post);
+  } catch {
+    res.status(500).json({ message: "The post could not be retrieved" });
+  }
+});
+
+router.delete("/:id", validatePostId, async (req, res) => {
+  try {
+    const deletedPost = await Posts.remove(req.post.id);
+    if (deletedPost) {
+      res.status(200).json(req.post);
+    } else {
+      res.status(500).json({ message: "The post could not be deleted" });
     }
+  } catch {
+    res.status(500).json({ message: "The post could not be deleted" });
+  }
 });
 
-router.get('/:id', validatePostId, (req, res) => {
-    res.send('/api/posts/:id')
-});
-
-router.delete('/:id', validatePostId, (req, res) => {
-    res.send('/api/posts/:id')
-});
-
-router.put('/:id', validatePostId, (req, res) => {
-    res.send('/api/posts/id')
+router.put("/:id", validatePostId, async (req, res) => {
+  try {
+    const updatedPost = await Posts.update(req.params.id, {
+      text: req.body.text,
+      user_id: req.post.user_id
+    });
+    if (updatedPost) {
+      const post = await Posts.getById(req.params.id);
+      res.status(200).json(post);
+    } else {
+      res.status(500).json({ message: "The post could not be updated" });
+    }
+  } catch {
+    res.status(500).json({ message: "The post could not be updated" });
+  }
 });
 
 // custom middleware
 
 async function validatePostId(req, res, next) {
-    const { id } = req.params;
-    if (Number.isInteger(parseInt(id, 10))) {
-      try {
-        const post = await Posts.getById(id);
-        if (post) {
-          req.post = post;
-          console.log(post);
-          next();
-        } else {
-          res
-            .status(404)
-            .json({ message: `The post with Id of '${id}' could not be found` });
-        }
-      } catch {
-        res.status(500).json({ message: "The post could not be retrieved" });
+  const { id } = req.params;
+  if (Number.isInteger(parseInt(id, 10))) {
+    try {
+      const post = await Posts.getById(id);
+      if (post) {
+        req.post = post;
+        next();
+      } else {
+        res
+          .status(404)
+          .json({ message: `The post with Id of '${id}' could not be found` });
       }
-    } else {
-      res.status(400).json({ message: `The Id of '${id}' is not valid` });
+    } catch {
+      res.status(500).json({ message: "The post could not be retrieved" });
     }
-};
+  } else {
+    res.status(400).json({ message: `The Id of '${id}' is not valid` });
+  }
+}
 
 module.exports = router;
