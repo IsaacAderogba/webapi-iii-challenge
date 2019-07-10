18b7351b01cc9b5dedd2e193bd1e2fc84e40610a
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-07-10 12:35:56 +0100

    Test userRouter endpoints on postman

--- users/userRouter.js
+++ users/userRouter.js
@@ -3,31 +3,32 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', (req, res) => {
-    res.send('/posts')
+    res.send('/api/users')
 });
 
 router.post('/:id/posts', (req, res) => {
-
+    res.send('/api/users/id/posts')
 });
 
 router.get('/', (req, res) => {
-
+    res.send('/api/users/')
 });
 
 router.get('/:id', (req, res) => {
-
+    res.send('/api/users/:id')
 });
 
 router.get('/:id/posts', (req, res) => {
+    res.send('/api/users/:id/posts')
 
 });
 
 router.delete('/:id', (req, res) => {
-
+    res.send('/api/users/:id')
 });
 
 router.put('/:id', (req, res) => {
-
+    res.send('/api/users/:id')
 });
 
 //custom middleware
